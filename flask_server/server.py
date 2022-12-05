c14cbb10671b799cd94d53139793f2a84b111eb6
betebetoven <albertoha2000@gmail.com>
2022-12-05 17:07:56 -0600

    gunicornadded

--- flask_server/server.py
+++ flask_server/server.py
@@ -46,71 +46,74 @@ def sofi():
         return send_file(nombre[len(nombre)-1]+".xlsx")
     
     return render_template('index.html', form = form)
-
-if __name__=="__main__":
-    print("HOLA MUNDO SI ESTA CORRIENDO")
-    #export FLASK_ENV=production
-    #export FLASK_APP=myapp
-    #gunicorn myapp:app
-    def progressBar(count, total, suffix=''):
+def progressBar(count, total, suffix=''):
 	    barLength = 60
 	    filledLength = int(round(barLength * count / float(total)))
 	    percent = round(100.0 * count / float(total), 1)
 	    bar = '=' * filledLength + '-' * (barLength - filledLength)
 	    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percent, '%', suffix))
 	    sys.stdout.flush()
-    def hoa():
-        return("probando ota vez el arumento inicial")
-    def sofi_traduce(direccion):
-        estados_cuentas = []
-        #direccion = askopenfilename()
-        nombre = direccion.split('/')
-        for n in range(4):
-            nombre[len(nombre)-1] = nombre[len(nombre)-1][:-1]
-        print(nombre[len(nombre)-1])
-        tabula.convert_into(direccion, "output.txt", output_format="csv", pages='all')
-        with open("output.txt", encoding = 'latin-1') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
-            my_list = []
-            cont = 1
-            progresocont = 0
-            tot = 0
-            print("Examinando PDF \n")
-            for row in csv_reader:
-                progressBar(progresocont,250)
-                progresocont = progresocont +1
-                tmp = []
-                strmp = '|'.join(row)
-                tmp = strmp.split('|')
-                palabrasReservadas = {'Departamento 5': "Department 5",
-                'Oper. Autom.': "Automatic Operation" ,
-                'Compensacion G': "Compensation G",
-                'Bca. Empresa': "Bca. Company" ,
-                'Cob.Cta.Ajena':"External account payment",
-                'Departamento 5B':"Department 5"
-                }
-                for n in tmp:
-                    salida = palabrasReservadas.get(n)
-                    if(salida != None):
-                        tmp[tmp.index(n)] = salida  
-                my_list.append(tmp)
-            progressBar(progresocont,progresocont)     
-            estados_cuentas.append(my_list)   
-            #workbook = xlsxwriter.Workbook(asksaveasfilename(initialfile=nombre[len(nombre)-1], defaultextension=".xlsx"))
-            workbook = xlsxwriter.Workbook(nombre[len(nombre)-1]+".xlsx")
-            worksheet = workbook.add_worksheet()
-            col = 0
-            print("\n")
-            print("ESCRIBIENDO XLCS \n")
-            for row, data in enumerate(my_list):
-                progressBar(row,len(my_list))
-                worksheet.write_row(row, col, data)
-            worksheet.write_row(len(my_list),0,"ESPACIO")
-            worksheet.default_col_width = 30
-            progressBar(len(my_list),len(my_list))
-            print("\nREALIZACION EXITOSA") 
-        workbook.close()
-        return nombre[len(nombre)-1]
+def hoa():
+    return("probando ota vez el arumento inicial")
+def sofi_traduce(direccion):
+    estados_cuentas = []
+    #direccion = askopenfilename()
+    nombre = direccion.split('/')
+    for n in range(4):
+        nombre[len(nombre)-1] = nombre[len(nombre)-1][:-1]
+    print(nombre[len(nombre)-1])
+    tabula.convert_into(direccion, "output.txt", output_format="csv", pages='all')
+    with open("output.txt", encoding = 'latin-1') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+        my_list = []
+        cont = 1
+        progresocont = 0
+        tot = 0
+        print("Examinando PDF \n")
+        for row in csv_reader:
+            progressBar(progresocont,250)
+            progresocont = progresocont +1
+            tmp = []
+            strmp = '|'.join(row)
+            tmp = strmp.split('|')
+            palabrasReservadas = {'Departamento 5': "Department 5",
+            'Oper. Autom.': "Automatic Operation" ,
+            'Compensacion G': "Compensation G",
+            'Bca. Empresa': "Bca. Company" ,
+            'Cob.Cta.Ajena':"External account payment",
+            'Departamento 5B':"Department 5"
+            }
+            for n in tmp:
+                salida = palabrasReservadas.get(n)
+                if(salida != None):
+                    tmp[tmp.index(n)] = salida  
+            my_list.append(tmp)
+        progressBar(progresocont,progresocont)     
+        estados_cuentas.append(my_list)   
+        #workbook = xlsxwriter.Workbook(asksaveasfilename(initialfile=nombre[len(nombre)-1], defaultextension=".xlsx"))
+        workbook = xlsxwriter.Workbook(nombre[len(nombre)-1]+".xlsx")
+        worksheet = workbook.add_worksheet()
+        col = 0
+        print("\n")
+        print("ESCRIBIENDO XLCS \n")
+        for row, data in enumerate(my_list):
+            progressBar(row,len(my_list))
+            worksheet.write_row(row, col, data)
+        worksheet.write_row(len(my_list),0,"ESPACIO")
+        worksheet.default_col_width = 30
+        progressBar(len(my_list),len(my_list))
+        print("\nREALIZACION EXITOSA") 
+    workbook.close()
+    return nombre[len(nombre)-1]
+    
+
+
+
+if __name__=="__main__":
+    print("HOLA MUNDO SI ESTA CORRIENDO")
+    #export FLASK_ENV=production
+    #export FLASK_APP=myapp
+    #gunicorn myapp:app
     
 
 
